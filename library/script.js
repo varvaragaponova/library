a4b1ab5fc48c082b3d8f5b0593faedf87165c36c
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2023-08-10 14:30:12 +0400

    fix: change burger menu close

--- library/script.js
+++ library/script.js
@@ -6,7 +6,8 @@ const body = document.querySelector('body');
 const burgerMenuBtn = document.querySelector(".burger_menu");
 const menu = document.querySelector(".menu_list");
 
-burgerMenuBtn.addEventListener('click', () => {
+burgerMenuBtn.addEventListener('click', (event) => {
+    event.stopPropagation();
     burgerMenuBtn.classList.toggle('menu_open');
     if (burgerMenuBtn.classList.contains('menu_open')) {
         menu.classList.add('menu_visible');
@@ -26,10 +27,23 @@ function onMenuClick(event) {
 
 body.addEventListener('click', closeMenu);
 
+// function closeMenu(event) {
+//     // event.stopPropagation();
+//     if (event.target.className === 'wrapper' || event.target.className === 'welcome_title' || event.target.className === 'logo') {
+//         burgerMenuBtn.classList.remove('menu_open');
+//         menu.classList.remove('menu_visible');
+//     }
+// }
+
 function closeMenu(event) {
-    // event.stopPropagation();
-    if (event.target.className === 'wrapper' || event.target.className === 'welcome_title' || event.target.className === 'logo') {
+    const { clientX, clientY } = event;
+    const { x, y } = menu.getBoundingClientRect();//метод позволять получить позиционирование элемента, т.е. его размеры и координаты по осям на страницы
+
+    if (clientX >= x && clientY >= y) return;//ловим, где была мышка и сравниваем с параметрами элемента
+
+    if (menu.classList.contains('menu_visible')) {
         burgerMenuBtn.classList.remove('menu_open');
         menu.classList.remove('menu_visible');
     }
+
 }
